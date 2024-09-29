0e03e961c4143a606c3f71df2d8656124cf4510d
RITIK RAJ <ritikraj40@gmail.com>
2024-09-29 12:23:27 +0530

    Add files via upload

---
+++ FirstBank.java
@@ -0,0 +1,15 @@
+public class FirstBank {
+
+	public static void main(String[] args) {
+
+		Account ankita = new Account("Ankita Panigrahi", "A00001");
+		
+		Account jagruti = new Account("Jagruti Hota", "J00001");
+
+		Account rakesh = new Account("Rakesh Sharma", "R00001");
+		rakesh.showMenu();
+		
+		
+	}
+
+}
